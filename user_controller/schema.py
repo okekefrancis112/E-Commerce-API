fdd4095525a809f00f68ad074b4bde5346df849b
Francis Chukwudalu Okeke <okeke98@gmail.com>
2022-12-17 19:32:02 +0100

    implemented user login

--- user_controller/schema.py
+++ user_controller/schema.py
@@ -1,6 +1,9 @@
 import graphene
 from .models import User
 from graphene_django import DjangoObjectType
+from django.contrib.auth import authenticate
+from datetime import datetime
+from ecommerce_api.authentication import TokenManager
 
 
 class UserType(DjangoObjectType):
@@ -23,6 +26,34 @@ class RegisterUser(graphene.Mutation):
         return RegisterUser(
             status=True,
             message="User created successfully"
+        )
+
+
+class LoginUser(graphene.Mutation):
+    access = graphene.String()
+    refresh = graphene.String()
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    def mutate(self, info, email, password):
+        user = authenticate(username=email, password=password)
+
+        if not user:
+            raise Exception("Invalid Credentials")
+
+        user.last_login = datetime.now()
+        user.save()
+
+        access = TokenManager.get_access({"user_id": user.id})
+        refresh = TokenManager.get_refresh({"user_id": user.id})
+
+        return LoginUser(
+            access=access,
+            refresh=refresh,
+            user=user,
         )
 
 
